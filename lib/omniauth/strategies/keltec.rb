15925333b5ef1d65e890e7152ac74c06807ba3c9
Nicolas Ganz <nicolas@keltec.ch>
2014-08-28 18:17:11 +0200

    added new info lines

--- lib/omniauth/strategies/keltec.rb
+++ lib/omniauth/strategies/keltec.rb
@@ -5,16 +5,20 @@ module OmniAuth
       option :name, :keltec
 
       option :client_options, {
-        :site => "http://accounts.m8proj.citrin.ch",
+        :site => 'http://accounts.m8proj.citrin.ch',
         #:port => 443,
-        :authorize_url => "/oauth/authorize"
+        :authorize_url => '/oauth/authorize'
       }
 
-      uid { raw_info["id"] }
+      uid { raw_info['id'] }
 
       info do
         {
-          :email => raw_info["email"]
+          email: raw_info['email'],
+          lastname: raw_info['lastname'],
+          surname: raw_info['surname'],
+          date_of_birth: raw_info['date_of_birth'],
+          image_url: raw_info['name']
         }
       end
 
