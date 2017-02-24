92df2e024b014322ad9cced1524f787acfb61fa2
Nicolas Ganz <nicolas@keltec.ch>
2017-02-24 23:08:13 +0100

    added fix for the "Invalid Credentials" error

--- lib/omniauth/strategies/keltec.rb
+++ lib/omniauth/strategies/keltec.rb
@@ -26,6 +26,10 @@ module OmniAuth
       def raw_info
         @raw_info ||= access_token.get('/api/v1/me.json').parsed
       end
+
+      def callback_url
+        full_host + script_name + callback_path
+      end
     end
   end
 end
